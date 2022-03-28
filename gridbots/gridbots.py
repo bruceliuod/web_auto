9f77a58eaeda3f96a877de7317d549e8ed53879a
bruceliuod <99635815+bruceliuod@users.noreply.github.com>
2022-03-28 17:28:17 +0800

    Add files via upload

--- gridbots/gridbots.py
+++ gridbots/gridbots.py
@@ -1,25 +1,26 @@
+
 import time
 import method
-from method import driver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-driver.get('https://testnet.bybit.com/zh-CN/tradingbot/')
-driver.maximize_window()
+method.driver.get('https://testnet.bybit.com/zh-CN/tradingbot/')
+method.driver.maximize_window()
+method.driver.implicitly_wait(30)
 method.login()
 
 # #首次创建
 # driver.find_element(By.CSS_SELECTOR,'._3qVRMTaiCTxNkwMRL62_ee._18N7vvAqcg4gp2TvpsTyA-').click()
-# time.sleep(3)
+# time.sleep(2)
 # method.create()
 
-# #已有机器人创建
-# driver.find_element(By.CSS_SELECTOR,'._3qVRMTaiCTxNkwMRL62_ee._2HUsEQ-QURXsT8vr_3R7_t').click()
-# time.sleep(3)
-# method.create()
+#已有机器人创建
+method.driver.find_element(By.CSS_SELECTOR,'._3qVRMTaiCTxNkwMRL62_ee._2HUsEQ-QURXsT8vr_3R7_t').click()
+time.sleep(2)
+method.create()
 
-#跟单1日榜第一
-method.copy()
+# #跟单1日榜第一
+# method.copy()
 
 # #我的机器人-详情-挂单-套利
 # method.running_bot()
--- gridbots/method.py
+++ gridbots/method.py
@@ -16,55 +16,60 @@ def login():
     driver.find_element(By.XPATH,'/html/body/div[2]/div/div/main/div/div/div/div[3]/div/div/div[2]/div/div[1]/div[2]/div[1]/div[2]/input').send_keys('Admin123456@@@')
     driver.find_element(By.XPATH,'/html/body/div[2]/div/div/main/div/div/div/div[3]/div/div/div[2]/div/div[2]/div/a').click()
     driver.switch_to.window(driver.window_handles[0])
+    time.sleep(2)
 
 #创建BTCUSDT机器人
 def create():
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
     driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/div[2]').click()
     driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[4]').click()
     driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/div[4]/div[1]/div[1]/input').send_keys('40000')
-    driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/div[4]/div[3]/div[1]/input').send_keys('45000')
+    driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/div[4]/div[3]/div[1]/input').send_keys('50000')
     driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/div[6]/input').send_keys('13')
-    driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/div[10]/input').send_keys('200')
+    driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/div[10]/input').send_keys('260')
     driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/button').click()
     driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[3]/div/button').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[1]/div/a').click()
+    time.sleep(2)
 
 #跟单1日榜第一
 def copy():
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div/div[3]/div/div[1]/div[2]/div/div[1]/div[2]/button').click()
     driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/div[4]/input').send_keys('500')
     driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[1]/button').click()
     driver.find_element(By.XPATH,'/html/body/div[2]/div[3]/div[2]/div[3]/div/button').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[1]/div/a').click()
+    time.sleep(2)
 
 #我的机器人-详情-挂单-套利
 def running_bot():
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div/div[2]/div/div[2]/div[2]/div[1]/div/a').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[2]/div/div/div/div/div[1]/div[2]/button').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[2]/div/div/ul/li[2]').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[2]/div/div/ul/li[3]').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[1]/div/a').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[1]/div/a').click()
+    time.sleep(2)
 
 #设置不允许跟单
 def follow():
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div/div[2]/div/div[2]/div[2]/div[1]/div/a').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[2]/div/div/div/div/div[1]/div[2]/button').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/div[2]').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[1]/div/a').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[1]/div/a').click()
+    time.sleep(2)
 
 #以close_mode 1 关闭机器人
 def close():
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div/div[2]/div/div[2]/div[2]/div[1]/div/a').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[2]/div/div/div/div/div[1]/div[2]/button').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/button[2]/span').click()
     driver.find_element(By.CSS_SELECTOR,'._3qVRMTaiCTxNkwMRL62_ee._4vx0Oa-Rl9yQujnDMwZ27').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[1]/div/a').click()
     driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[1]/div/a').click()
-    
+    time.sleep(2)
